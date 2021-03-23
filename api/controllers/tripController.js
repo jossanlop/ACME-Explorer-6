1213fa5eb17f126777607d2b2381b6dea709c0c5
jossanlop <jose.enrique.sanchez@outlook.com>
2021-03-23 20:53:06 +0100

    trip role authentication added

--- api/controllers/tripController.js
+++ api/controllers/tripController.js
@@ -88,7 +88,7 @@ exports.list_all_trips = function(req, res) {
 };
 
 exports.create_an_trip = function(req, res) {
-  //Check if the user is an administrator and if not: res.status(403); "an access token is valid, but requires more privileges"
+  //Check if the user is a MANAGER and if not: res.status(403); "an access token is valid, but requires more privileges"
   var new_trip = new Trip(req.body);
   new_trip.save(function(err, trip) {
     if (err){
@@ -127,7 +127,9 @@ exports.read_an_trip = function(req, res) {
 };
 
 exports.update_an_trip = function(req, res) {
-  //Check that the user is administrator if it is updating more things than comments and if not: res.status(403); "an access token is valid, but requires more privileges"
+  //Check that the user is MANAGER if it is updating more things than comments and if not: res.status(403); "an access token is valid, but requires more privileges"
+
+  
     Trip.findOneAndUpdate({ticker: req.params.tripId}, req.body, {new: true, runValidators:true }, function(err, Trip) {
       if (err){
         if(err.name=='ValidationError') {
@@ -144,7 +146,7 @@ exports.update_an_trip = function(req, res) {
 };
 
 exports.delete_an_trip = function(req, res) {
-  //Check if the user is an administrator and if not: res.status(403); "an access token is valid, but requires more privileges"
+  //Check if the user is an MANAGER and if not: res.status(403); "an access token is valid, but requires more privileges"
   Trip.deleteOne({ticker: req.params.tripId}, function(err, Trip) {
         if (err){
             res.status(500).send(err);
@@ -154,75 +156,3 @@ exports.delete_an_trip = function(req, res) {
         }
     });
 };
-
-
-/*---------------Application----------------------*/
-// var mongoose = require('mongoose'),
-// Application = mongoose.model('Applications');
-
-// exports.list_all_Applications = function(req, res) {
-//   Application.find({}, function(err, apps) {
-//     if (err){
-//       res.status(500).send(err);
-//     }
-//     else{
-//       res.json(apps);
-//     }
-//   });
-// };
-
-// exports.create_a_Application = function(req, res) {
-//   var new_app = new Application(req.body);
-//   new_app.save(function(err, app) {
-//     if (err){
-//       if(err.name=='ValidationError') {
-//           res.status(422).send(err);
-//       }
-//       else{
-//         res.status(500).send(err);
-//       }
-//     }
-//     else{
-//       res.json(categ);
-//     }
-//   });
-// };
-
-
-// exports.read_a_Application = function(req, res) {
-//   Application.findById(req.params.categId, function(err, categ) {
-//     if (err){
-//       res.status(500).send(err);
-//     }
-//     else{
-//       res.json(categ);
-//     }
-//   });
-// };
-
-// exports.update_a_Application = function(req, res) {
-//   Application.findOneAndUpdate({_id: req.params.categId}, req.body, {new: true}, function(err, categ) {
-//     if (err){
-//       if(err.name=='ValidationError') {
-//         res.status(422).send(err);
-//       }
-//       else{
-//         res.status(500).send(err);
-//       }
-//     }
-//     else{
-//       res.json(categ);
-//   }
-//   });
-// };
-
-// exports.delete_a_Application = function(req, res) {
-//   Application.deleteOne({_id: req.params.categId}, function(err, categ) {
-//     if (err){
-//       res.status(500).send(err);
-//     }
-//     else{
-//       res.json({ message: 'Application successfully deleted' });
-//     }
-//   });
-// };
