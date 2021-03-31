43c7d60dbe6c5ae98ad786db5b557a555c5d97b6
jossanlop <jose.enrique.sanchez@outlook.com>
2021-03-31 12:43:30 +0200

    autenticacion por roles sponsorship

--- api/routes/sponsorshipRoutes.js
+++ api/routes/sponsorshipRoutes.js
@@ -3,11 +3,10 @@ module.exports = function(app) {
   var sponsorships = require('../controllers/sponsorshipController');
 
   /**
-   * Get an sponsorship who is clerk (any role)
-   *    Required role: Administrator
-   * Post an sponsorship 
+   * Get a sponsorship 
+   *    Required role: None
+   * Post a sponsorship 
    *    RequiredRoles: None
-   *    validated if customer and not validated if clerk
 	 *
 	 * @section sponsorships
 	 * @type get post
@@ -17,19 +16,51 @@ module.exports = function(app) {
 	  .get(sponsorships.list_all_sponsorships)
 	  .post(sponsorships.create_an_sponsorship);
 
-  /**
-   * Put an sponsorship
-   *    RequiredRoles: to be the proper sponsorship
-   * Get an sponsorship
-   *    RequiredRoles: to be the proper sponsorship or an Administrator
+/**
+   * Get all sponsorships
+   *    Required role: Administrator
+   * Post a sponsorship 
+   *    RequiredRoles: Administrator, Sponsor
 	 *
 	 * @section sponsorships
-	 * @type get put
+	 * @type get post
+	 * @url /v2/sponsorships
+*/
+   app.route('/v2/sponsorships')
+    .get(authController.verifyUser(["ADMINISTRATOR"]),sponsorships.list_all_sponsorships)
+    .post(authController.verifyUser(["ADMINISTRATOR"]),sponsorships.create_an_sponsorship);
+
+  /**
+   * Put a sponsorship
+   *    RequiredRoles: none
+   * Get a sponsorship
+   *    RequiredRoles: none
+	 *
+	 * @section sponsorships
+	 * @type get put delete
 	 * @url /v1/sponsorships/:sponsorshipId
   */  
   app.route('/v1/sponsorships/:sponsorshipId')
     .get(sponsorships.read_an_sponsorship)
-	.put(sponsorships.update_an_sponsorship)
+	  .put(sponsorships.update_an_sponsorship)
     .delete(sponsorships.delete_an_sponsorship);
+
+
+    /**
+   * Put a sponsorship
+   *    RequiredRoles: to be the proper Sponsor
+   * Get a sponsorship
+   *    RequiredRoles: to be the proper Sponsor or an Administrator
+   * Delete a sponsorship
+   *    RequiredRoles: to be the proper Sponsor
+	 *
+	 * @section sponsorships
+	 * @type get put delete
+	 * @url /v2/sponsorships/:sponsorshipId
+  */  
+  app.route('/v2/sponsorships/:sponsorshipId')
+    .get(authController.verifyUser(["SPONSORS","ADMINISTRATOR"]),sponsorships.read_an_sponsorship)
+    .put(authController.verifyUser(["SPONSORS"]),sponsorships.update_an_sponsorship)
+    .delete(authController.verifyUser(["SPONSORS"]),sponsorships.delete_an_sponsorship);
 
 }
